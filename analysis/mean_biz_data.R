8f1299e62a3b6faa0ba8b72d7909988bedf6e70d
tomplah <tomplah@comcast.net>
2014-02-19 14:27:35 -0500

    Updated R code to use long format for business data

--- analysis/mean_biz_data.R
+++ analysis/mean_biz_data.R
@@ -6,7 +6,7 @@ library(RPostgreSQL)
 library(gdata)
 
 #Path 
-setwd('/home/tom/R')
+setwd('/home/tom/dataswap_tax/analysis')
 
 # Connect to database
 drv <- dbDriver("PostgreSQL")
@@ -28,21 +28,38 @@ for(yr in 2007:2011){
 
 dbDisconnect(con) # Close the connection
 
-#Combine years, full outer join, replace NAs with 0s (wont upload to PostgreSQL otherwise)
+#Combine years, inner join only, replace NAs with 0s (won't upload to PostgreSQL otherwise)
 mv = get('biz2007')
 for(yr in 2008:2011){
   toadd = get(paste('biz',yr,sep=''))
-    mv = merge(mv, toadd, by = "GEOID10",all = T)
+    mv = merge(mv, toadd, by = "GEOID10")
     mv[is.na(mv)] <- 0
     }
 
+#Reshape dataframe to long format by GEOID10 and year
+mvlong <-reshape(mv, 
+             idvar = "GEOID10", 
+             varying = list(
+               c("sales_vol_2007", "sales_vol_2008", "sales_vol_2009", "sales_vol_2010", "sales_vol_2011"),
+               c("number_emp_2007", "number_emp_2008", "number_emp_2009", "number_emp_2010", "number_emp_2011"),
+               c("sic_2007", "sic_2008", "sic_2009", "sic_2010", "sic_2011")),
+                v.names = c("mean_salesvol", "mean_numberemp", "count_firms"),
+             times = c(2007, 2008, 2009, 2010, 2011),
+             timevar = "year",
+             direction = "long") 
+            
+      attach(mvlong)
+      mvlong_sorted <-mvlong[order(GEOID10,year),]
+      detach(mvlong)
+      row.names(mvlong_sorted) <- NULL
+      
 #Save data as .csv
-write.csv(mv, "mean_values_biz.csv", row.names = F)
+write.csv(mvlong_sorted, "mean_values_biz.csv", row.names = F)
 
 #Write dataframe to Postgres
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, host="66.228.36.34",user='dataswap',password = 'taxtaxtax',dbname="dataswap", port="5432")
     dbRemoveTable(con, c("aggregated","biz_data"))
-    dbWriteTable(con, c("aggregated","biz_data"), value=mv)
+    dbWriteTable(con, c("aggregated","biz_data"), value=mvlong_sorted)
 dbDisconnect(con)
 
